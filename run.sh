9f424ed5e7718660a5c612b4da04b618407c2c38
Hyunwook Kang <kangtaepoong@naver.com>
2026-01-16 17:08:02 +0900

    Delete run.sh

--- run.sh
+++
@@ -1,2 +0,0 @@
-FEIS=/home/Hyunwook/codes/BrainCon-revision/FEIS-v1.1/scottwellington-FEIS-7e726fd/experiments
-python main.py --model_name Conformer --dataset_dir $FEIS --num_electrodes 14 --n_subjects 21 --exper-setting indep --save_file_name indep_conformer_results.csv
